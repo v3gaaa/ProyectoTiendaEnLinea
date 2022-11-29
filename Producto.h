8706aa4d6eb926e40b4c7d31c63c26df401803c6
Spiegelin <100853421+Spiegelin@users.noreply.github.com>
2022-11-29 17:31:15 -0600

    Update Producto.h

---
+++ Producto.h
@@ -0,0 +1,23 @@
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+class Producto{
+    protected:
+        string nombre;
+        double precio;
+        int cantidad;
+    public:
+        Producto();
+        Producto(string, double, int);
+        ~Producto();
+        void editProducto();
+        string getNombreProducto();
+        double getPrecioProducto();
+        int getCantidadProducto();
+};
+
+#endif
