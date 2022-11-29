f696c8c3b62037e5d06e808ff82bf952151d1bb3
Spiegelin <100853421+Spiegelin@users.noreply.github.com>
2022-11-29 17:32:13 -0600

    Update Pedido.cpp

---
+++ Pedido.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include "Pedido.h"
+using namespace std;
+
+// Constructor
+Pedido::Pedido(int _pedidoID, string _detallesEnvio, double _totalCompra){
+    pedidoID = _pedidoID;
+    detallesEnvio = _detallesEnvio;
+    totalCompra = _totalCompra;
+}
+
+// Destructor
+Pedido::~Pedido(){
+}
+
+// Finalizar Pedido
+void Pedido::finalizarPedido(){
+}
+
+int Pedido::getPedidoID(){
+    return pedidoID;
+}
