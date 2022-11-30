175db09ef031b120e84f5ede7ce383d49cd105dc
Pablo H <65913304+Pablo389@users.noreply.github.com>
2022-11-30 22:24:37 +0000

    Funcion iniciarPedido

--- Pedido.cpp
+++ Pedido.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <vector>
 #include "Pedido.h"
+#include "Producto.cpp"
 using namespace std;
 
 // Constructor
@@ -16,8 +18,20 @@ Pedido::~Pedido(){
 }
 
 // Finalizar Pedido
-void Pedido::finalizarPedido(){
+void Pedido::iniciarPedido(vector<Producto> _ProductosCarrito){
+    for (int i; i< _ProductosCarrito.size(); i++){
+        cout<<_ProductosCarrito[i].getNombreProducto()<<" ";
+        cout<<_ProductosCarrito[i].getPrecioProducto()<< " ";
+        cout<<_ProductosCarrito[i].getNombreProducto()<< " ";
+        cout<<endl;
+    }
 
+    cout<<"SI todo los productos estan bien por favor ingresa un 1, si no, ingresa 0"<<endl;
+    int input;
+    cin>>input;
+    if (input == 1){
+        cout<<"El pedido esta hecho";
+    }
 }
 
 int Pedido::getPedidoID(){
